68fab80de759fd80db9922c0654821c8b6a189ca
Andrew Jorgensen <andrew@andrewjorgensen.com>
2018-12-02 11:03:33 -0500

    Implement day 1 part two

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     let contents = fs::read_to_string("frequencies.txt")
@@ -9,9 +10,33 @@ fn main() {
         .map( |line| line.parse::<i32>().unwrap() )
         .collect();
 
-    let result = values
-        .iter()
-        .fold(0, |acc, x| acc + x);
+    println!("Part One: {}", part_one(&values));
+    println!("Part Two: {}", part_two(&values));
+}
 
-    println!("{:?}", result);
+fn part_one(values: &Vec<i32>) -> i32 {
+    values.iter().fold(0, |acc, x| acc + x)
+}
+
+fn part_two(values: &Vec<i32>) -> i32 {
+    let mut dup_detection: HashSet<i32> = HashSet::new();
+    let mut found = false;
+    let mut freq = 0;
+
+    dup_detection.insert(freq);
+
+    while !found {
+        for value in values {
+            freq += value;
+
+            if dup_detection.contains(&freq) {
+                found = true;
+                break;
+            } else {
+                dup_detection.insert(freq.clone());
+            }
+        }
+    }
+
+    freq
 }
